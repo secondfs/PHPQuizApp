52bb554425b146266936307c46d3c51b912906af
secondfs <rikors43@gmail.com>
2021-11-10 13:35:16 +0200

    init

---
+++ database/migrations/2021_11_09_144117_create_passings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePassingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('passings', function (Blueprint $table) {
+            $table->id();
+            $table->string('nickname')->unique();
+            $table->integer('correct_answers')->nullable();
+            $table->integer('total_answers');// Should take this from app config
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('passings');
+    }
+}
