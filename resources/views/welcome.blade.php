dfdf31c41d6d31d5ed6afc6d9fd8b8da8a3000cd
secondfs <rikors43@gmail.com>
2021-12-15 19:15:41 +0200

    fixed mysql connect

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     <div class="d-flex align-items-center justify-content-center">
-        <button type="submit" id="startQuiz" class="mt-5 d-flex btn btn-primary border-success align-items-center btn-success"> Start <i class="fa fa-angle-right ml-2"></button>
+        <button type="submit" id="startQuiz" class="mt-5 d-flex btn btn-primary border-success align-items-center btn-success"> Начать <i class="fa fa-angle-right ml-2"></button>
     </div>
 
     <x-forms.nickname-form></x-forms.nickname-form>
